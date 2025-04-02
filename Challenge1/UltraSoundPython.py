2b1012b0f40a8128fc81eb5650ae78c687d98599
yazz3660 <145766114+yazz3660@users.noreply.github.com>
2025-04-02 11:03:56 +0100

    Update UltraSoundPython.py
    Checks if string from arduino is equal to ultrasound, if it is then the robot will stop

--- Challenge1/UltraSoundPython.py
+++ Challenge1/UltraSoundPython.py
@@ -1,5 +1,12 @@
 # Importing Libraries
 import serial
+import tkinter as tk
+from tkinter import messagebox
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+import threading
 # Change for your com port, e.g.: "COM8:", "/dev/ttyUSB0", etc.
 arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
 
@@ -16,6 +23,13 @@ while ( True ):
                 print("Arduino was Reset")
             else:
                 print(value_str) 
+                if value_str == "ultrasound":
+                    twist_msg.linear.x = 0.0  # Stop if an obstacle is too close
+                    twist_msg.angular.z = 0.0
+                    self.get_logger().info('Obstacle detected! Stopping.')
+                    print("Stopping")
+                    self.publisher.publish(twist_msg)
+
     except UnicodeDecodeError as e:
         print(f"Decoding error: {e}")
 
